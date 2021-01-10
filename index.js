20d9166656dae32b77d565a3cd56ba8980d8c3ea
Gui Prá <super.driver.512@gmail.com>
2021-01-10 08:17:12 +0100

    .

--- index.js
+++ index.js
@@ -1,6 +1,62 @@
 'use strict';
 
+let boundNodes = [];
+
+let ariaDataRegExp = /^(aria|data)-/;
+let svgNsRegExp = /\/svg$/;
+let wsRegExp = / |\r|\n/;
+
+function nullish(x) { return x === undefined || x === null }
+
+// Possible class values:
+// nullish, numbers;
+// single-class strings (e.g. 'foo');
+// multi-class strings (whitespace separated, e.g. 'foo  bar\n baz');
+// (nested) arrays of any of the above.
+// This function normalizes all possibilities to arrays of single-class strings.
+// Note that booleans are always filtered out (so we can use short-circuit
+// operators without accidentally adding classes like 'true' or 'false').
+function normalizeClasses(x) {
+  if (!Array.isArray(x)) { x = [x] }
+
+  return flatMap(x, function(y) {
+    return (y || typeof y === 'number') && String(y).split(wsRegExp);
+  }).filter(Boolean);
+}
+
+// All functions accepting child node values use this function to convert
+// strings and numbers to text nodes. Booleans are converted to null (for
+// basically the same reason as described above in normalizeClasses).
+function appendableNode(x) {
+  if (x instanceof Node) { return x }
+  if ((!x && typeof x !== 'number') || typeof x === 'boolean') { return null }
+
+  return document.createTextNode(x);
+}
+
+// Some nodes have n.anchoredNodes. When removing n, it's important to also
+// removed all anchored nodes. This function can be safely used to remove any
+// node, even ones that don't have anchored nodes.
+function removeWithAnchoredNodes(n) {
+  let i;
+
+  if (n.anchoredNodes) {
+    for (i = 0; i < n.anchoredNodes.length; i++) {
+      n.anchoredNodes[i].remove();
+    }
+
+    delete n.anchoredNodes;
+  }
+
+  n.remove();
+}
+
+// All bindings are represented as instances of this class.
 function Binding(x) {
+  // Incorporate all key-values in x when x is an object.
+  // typeof array === 'object', but arrays are to be stored as this.get
+  // (e.g. class: new Binding([fn1, fn2...])), that's why we need the extra
+  // check.
   if (typeof x === 'object' && !Array.isArray(x)) {
     objAssign(this, x);
   } else {
@@ -8,87 +64,112 @@ function Binding(x) {
   }
 }
 
-let ariaDataRegExp = /^(aria|data)-/;
-let svgNsRegExp = /\/svg$/;
-
+// Most prop bindings can be updated in a unified fashion:
 Binding.prototype.update = function() {
   let el = this.target, newValue = this.get();
 
-  if (newValue !== this.lastValue) {
-    if (ariaDataRegExp.test(this.key) || svgNsRegExp.test(el.namespaceURI)) {
-      if (newValue === undefined || newValue === null) {
-        el.removeAttribute(this.key);
-      } else {
-        el.setAttribute(this.key, newValue);
-      }
+  // If the value hasn't changed, do nothing.
+  if (newValue === this.lastValue) { return }
+
+  // aria-*, data-*, and SVG element props need to be managed as attributes.
+  if (ariaDataRegExp.test(this.key) || svgNsRegExp.test(el.namespaceURI)) {
+    if (!nullish(newValue)) {
+      el.setAttribute(this.key, newValue);
     } else {
-      el[this.key] = newValue;
+      el.removeAttribute(this.key);
     }
+  } else {
+    // All else are regular DOM element properties.
+    el[this.key] = newValue;
   }
 
+  // Remember updated value.
   this.lastValue = newValue;
 };
 
+// Some prop bindings like class, style, and value need special handling.
+// The bindToNode function (below) will automatically set these special update
+// handlers to the appropriate Binding instances, overriding the default one
+// (see above). These can also be overridden or extended if necessary by
+// patching Binding.specialUpdateFnsByKey.
 Binding.specialUpdateFnsByKey = {
   class: function classBindingUpdate() {
+    // Supports (nested) array/non-array class value getters.
     let i, x, el = this.target, newValue = Array.isArray(this.get)
       ? flatMap(binding.get, function(x) { return x() })
-      : binding.get();
+      : this.get();
 
-    if (typeof newValue === 'string') { newValue = [newValue] }
-
-    newValue = flatMap(newValue, function(x) {
-      return x && String(x).split(/ |\r|\n/);
-    }).filter(Boolean);
+    newValue = normalizeClasses(newValue);
 
     this.lastValue = this.lastValue || [];
 
+    // Remove classes in lastValue but not in newValue.
     for (i = 0; i < this.lastValue.length; i++) {
       x = this.lastValue[i];
       if (newValue.indexOf(x) === -1) { el.classList.remove(x) }
     }
 
+    // Added classes in newValue but not in lastValue.
     for (i = 0; i < newValue.length; i++) {
       x = newValue[i];
       if (lastValue.indexOf(x) === -1) { el.classList.add(x) }
     }
 
+    // Remember updated value.
     this.lastValue = newValue;
   },
 
   style: function styleBindingUpdate() {
     let newValue = this.get();
 
-    if (newValue !== this.lastValue) {
-      this.target.style[this.subkey] =
-        newValue !== undefined && newValue !== null ? newValue : '';
-    }
+    // If the value hasn't changed, do nothing.
+    if (newValue === this.lastValue) { return }
 
+    // Nullish values are converted to empty strings, everything else is
+    // assigned as-is (the browser itself converts them to strings).
+    this.target.style[this.subkey] = !nullish(newValue) ? newValue : '';
+
+    // Remember updated value.
     this.lastValue = newValue;
   },
 
   value: function valueBindingUpdate() {
     let newValue;
 
+    // On first update, lazily creates event handlers for tracking input value
+    // changes.
     if (!this.setHandler) {
       this.target.addEventListener('keyup', this.setHandler = function(ev) {
         let x = ev.target.value;
         this.lastValue = this.set ? this.set(x) : x;
-        update();
+
+        // Calling this.set inherently changes application state, so we may
+        // need to update other bindings elsewhere that depend on it.
+        this.set && update();
       });
     }
 
     if (this.get) {
       newValue = this.get();
 
-      if (newValue !== this.lastValue) { this.target.value = newValue }
-      this.lastValue = newValue;
+      // Convert nullish and boolean values to empty strings. Cast everything
+      // else to string.
+      if (nullish(newValue) || typeof newValue === 'boolean') { newValue = '' }
+      else { newValue = String(newValue) }
+
+      // If the value hasn't changed, do nothing.
+      if (newValue === this.lastValue) { return }
+
+      // Update element and remember updated value.
+      this.lastValue = this.target.value = newValue;
     }
   }
 };
 
 function createBinding(x) { return new Binding(x) }
 
+// Initializes common binding props (target, key, subkey, update) and adds
+// bindings to DOM nodes.
 function bindToNode(n, key, subkey, binding) {
   let bindingUpdateFn = Binding.specialUpdateFnsByKey[key];
 
@@ -103,7 +184,7 @@ function createElement(type) {
 
   let rest = [].slice.call(arguments, 1);
 
-  let props = rest[0] === undefined || rest[0] === null ||
+  let props = nullish(rest[0]) ||
     (rest[0] && rest[0].constructor === Object) ? rest.shift() : {};
 
   // Flatten children arrays.
@@ -172,10 +253,7 @@ function createElement(type) {
 
           // Convert remaining values to strings and statically add them to the
           // element.
-          el.classList.add.apply(
-            el.classList,
-            String(v2).split(/ |\r|\n/).filter(Boolean)
-          );
+          el.classList.add.apply(el.classList, normalizeClasses(v2));
         }
 
         continue;
@@ -192,7 +270,7 @@ function createElement(type) {
 
     // Special handling for style props.
     if (k === 'style') {
-      // If prop value is an object, assign all key-value pairs to el.style.
+      // Special handling for objects.
       if (typeof v === 'object') {
         for (k2 in v) {
           if (!v.hasOwnProperty(k2)) { continue }
@@ -227,9 +305,8 @@ function createElement(type) {
 
   // Append children (if any).
   for (i = 0; i < children.length; i++) {
-    v = children[i];
-    v = typeof v === 'string' ? document.createTextNode(v) : v;
-    el.appendChild(v);
+    v = appendableNode(children[i]);
+    v && el.appendChild(v);
   }
 
   // Return newly created element.
@@ -237,7 +314,7 @@ function createElement(type) {
 }
 
 function createComment(text) {
-  return document.createComment(text ? ' ' + text + ' ' : ' ');
+  return document.createComment(!nullish(text) ? ' ' + text + ' ' : ' ');
 }
 
 function createBoundComment(text, bindingProps) {
@@ -256,51 +333,50 @@ function createIfAnchor(predFn, thenNode, elseNode) {
 }
 
 function ifAnchorBindingUpdate() {
-  let i, n, parentEl, nAnchor = this.target, newValue = Boolean(this.get());
+  let i, n, parentEl;
+  let nAnchor = this.target, newValue = Boolean(this.get()), nNew, nCursor;
 
-  if (this.lastValue === undefined || newValue !== this.lastValue) {
+  if (newValue !== this.lastValue || this.lastValue === undefined) {
     parentEl = nAnchor.parentElement;
 
-    if (parentEl) {
-      for (let n of nAnchor.anchoredNodes || []) {
-        exports.remove(n); // FIXME
-      }
+    for (i = 0; i < (nAnchor.anchoredNodes || []).length; i++) {
+      removeWithAnchoredNodes(nAnchor.anchoredNodes[i]);
+    }
 
-      nAnchor.anchoredNodes = [];
+    nAnchor.anchoredNodes = [];
 
-      let nNew = newValue ? binding.thenNode : binding.elseNode;
+    if (!parentEl) { return }
 
-      if (nNew) {
-        let nCursor = nAnchor;
+    nNew = newValue ? this.thenNode : this.elseNode;
 
-        for (let n of Array.isArray(nNew) ? nNew : [nNew]) {
-          if (!(n instanceof Node)) {
-            n = document.createTextNode(n);
-          }
+    if (nNew) {
+      nCursor = nAnchor;
+      nNew = Array.isArray(nNew) ? nNew : [nNew];
 
-          parentEl.insertBefore(n, nCursor.nextSibling);
-          nAnchor.anchoredNodes.push(n);
+      for (i = 0; i < nNew.length; i++) {
+        n = appendableNode(nNew[i]);
+        if (!n) { continue }
 
-          nCursor = n;
+        parentEl.insertBefore(n, nCursor.nextSibling);
+        nAnchor.anchoredNodes.push(n);
 
-          for (let nAnchored of n.anchoredNodes || []) {
-            parentEl.insertBefore(nAnchored, nCursor.nextSibling);
-            nCursor = nAnchored;
-          }
-        }
+        nCursor = n;
       }
     }
   }
 
-  binding.lastValue = newValue;
+  this.lastValue = newValue;
 }
 
 function createMapAnchor(getFn, mapFn) {
   return createBoundComment('map anchor', {
     get: getFn,
     map: mapFn,
-    update: mapAnchorUpdate
+    update: mapAnchorBindingUpdate
   });
+}
+
+function mapAnchorBindingUpdate() {
 }
 
 function createTextNode(getFn) {
@@ -308,8 +384,6 @@ function createTextNode(getFn) {
   n.bindings = [new Binding({ get: getFn, update: textNodeUpdate })];
   return n;
 }
-
-let boundNodes = [];
 
 // TODO: Workaround for IE11's broken TreeWalker.
 function forEachNodeWithBindings(ns, cb) {
