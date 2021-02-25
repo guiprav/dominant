1f95efd0614e25d39a6fecbe65898050b0acba03
Gui Prá <gui@supersaas.com>
2021-02-25 05:04:37 +0100

    .

--- demos/build.sh
+++ demos/build.sh
@@ -2,5 +2,5 @@
 
 for dir in */; do
   echo "Building demos/$dir..."
-  npx browserify -t babelify "$dir/index.jsx" -o "$dir/bundle.js"
+  npx browserify -dt babelify "$dir/index.jsx" -o "$dir/bundle.js"
 done
